6d2c27030b4f5aa71a99524aa88fb333d2bd7714
tiziasan <tizianosantilli@gmail.com>
2020-02-28 17:24:04 +0100

    inital commit

---
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/GymportalApplication.java
@@ -0,0 +1,13 @@
+package it.univaq.disim.mwt.gymportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GymportalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GymportalApplication.class, args);
+	}
+
+}
---
+++ gymportal/src/test/java/it/univaq/disim/mwt/gymportal/GymportalApplicationTests.java
@@ -0,0 +1,13 @@
+package it.univaq.disim.mwt.gymportal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymportalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
