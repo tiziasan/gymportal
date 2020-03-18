8ffed8db1abdfd125a61cb1408da880925f434f9
tiziasan <tizianosantilli@gmail.com>
2020-03-18 17:46:24 +0100

    Added create course and create gym

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/presentation/RegionController.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/presentation/RegionController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("")
 
 public class RegionController {
+	
      
      @GetMapping("/abruzzo")
      public String listAb(Model model) {
