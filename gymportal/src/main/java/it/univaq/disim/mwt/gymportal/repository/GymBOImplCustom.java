5bf03b4571bfa60c5fbd3801545492cc63ab6f9b
fons94 <djpoldowlf@gmail.com>
2020-03-23 23:06:29 +0100

    add query for findByRegion

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/repository/GymBOImplCustom.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/repository/GymBOImplCustom.java
@@ -7,7 +7,7 @@ import org.springframework.data.jpa.repository.Query;
 import it.univaq.disim.mwt.gymportal.domain.Gym;
 
 public interface GymBOImplCustom {
-	@Query(value = "SELECT gym.region FROM gym ")
+	@Query(value = "FROM Gym AS g WHERE g.region = :region")
 	public List<Gym> findByRegionName(String region);
 	
 }
