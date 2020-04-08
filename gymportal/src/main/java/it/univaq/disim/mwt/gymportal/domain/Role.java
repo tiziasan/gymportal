ea1b65175eb2e86850389514d7d0778a00256e6e
fons94 <djpoldowlf@gmail.com>
2020-04-08 15:02:39 +0200

    add rule_user

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Role.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Role.java
@@ -1,10 +1,15 @@
 package it.univaq.disim.mwt.gymportal.domain;
 
+import java.util.List;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
@@ -29,6 +34,9 @@ public class Role {
 	@Size(max=200)
 	@Column(name="DESCRIPTION")
 	private String description;
+	
+	@OneToMany(mappedBy="role")   
+	private List<User> user;
 
 
 }
--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/User.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/User.java
@@ -7,6 +7,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -58,5 +61,12 @@ public class User {
 	
 	@OneToMany(mappedBy="user")
 	private List<FeedbackGym> feedbackGym;
+	
+	@ManyToOne
+	@JoinTable(name = "role_user",             
+	joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id") ,             
+	inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+	
+	private Role role;
 	
 }
