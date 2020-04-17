b9ee34d45b488ad3e6890a048fdcb887597cfab5
AposVinc <apos.vincenzo@gmail.com>
2020-04-17 21:37:48 +0200

    Update Message.java

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Message.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Message.java
@@ -5,7 +5,6 @@ import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 
-import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
 
@@ -15,12 +14,17 @@ public class Message {
 
     @Id
     private Long Id;
-/*
-    @Field("date")
-    //controlla tipo
-    private LocalDateTime date;
-*/
+
+    @Field("mittente")
+    private Long idMittente;
+
+    @Field("destinatario")
+    private Long idDestinatario;
+
     @Field("message")
     private String message;
 
+    @Field("date")
+    //controlla tipo
+    private LocalDateTime date;
 }
