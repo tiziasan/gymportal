dc495c946dbf9cbb69797c38c5d125879fd5df08
fons94 <djpoldowlf@gmail.com>
2020-04-19 17:00:15 +0200

    added owner on the gym

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Gym.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Gym.java
@@ -7,6 +7,7 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
@@ -51,4 +52,7 @@ public class Gym {
 	@OneToMany(mappedBy="gym")
 	private List<Course> course;
 
+	@NotBlank
+	@ManyToOne
+	private User owner;
 }
