d3491fb8441fb051498eb9f57a3a1fe65faae6dc
AposVinc <apos.vincenzo@gmail.com>
2020-09-24 17:15:31 +0200

    .

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/business/ChatBO.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/business/ChatBO.java
@@ -7,13 +7,17 @@ import it.univaq.disim.mwt.gymportal.domain.Chat;
 public interface ChatBO {
 
     Chat saveChat(Chat chat) throws BusinessException;
+
     void saveAllChats(List<Chat> chatList) throws BusinessException;
 
     void deleteChatsByGymId(long gymId) throws BusinessException;
 
     Chat findChatById(String id) throws BusinessException;
+
     List<Chat> findByUserId(long userId) throws BusinessException;
+
     List<Chat> findByGymId(long gymId) throws BusinessException;
+
     Chat findByUserIdAndGymId(long userId, long gymId) throws BusinessException;
 
 
--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/business/MessageBO.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/business/MessageBO.java
@@ -8,6 +8,7 @@ import it.univaq.disim.mwt.gymportal.domain.Message;
 public interface MessageBO {
 
     Message createMessage (Message msg) throws BusinessException;
+
     List<Message> findByChat(Chat chat) throws BusinessException;
 
     void deleteMessagesByChat(Chat chat) throws BusinessException;
