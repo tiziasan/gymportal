bd934ae1cca547d22babc9f0e02844709f98d0a6
AposVinc <apos.vincenzo@gmail.com>
2020-09-25 22:05:50 +0200

    Update Role.java

--- gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Role.java
+++ gymportal/src/main/java/it/univaq/disim/mwt/gymportal/domain/Role.java
@@ -2,18 +2,18 @@ package it.univaq.disim.mwt.gymportal.domain;
 
 public enum Role {
 
-    CUSTOMER("C"),
-    MANAGER("M");
+    CUSTOMER("CUSTOMER"),
+    MANAGER("MANAGER");
 
     private String value;
 
     private Role(String role) {
         switch (role){
-            case "M":
-                setValue("M");
+            case "MANAGER":
+                setValue(role);
                 break;
-            case "C":
-                setValue("C");
+            case "CUSTOMER":
+                setValue(role);
                 break;
             default:
                 throw new IllegalArgumentException("Incorrect use of Role");
@@ -29,8 +29,39 @@ public enum Role {
     }
 
     public static class Values {
-        public static final String CUSTOMER= "C";
-        public static final String MANAGER= "M";
+        public static final String CUSTOMER= "CUSTOMER";
+        public static final String MANAGER= "MANAGER";
     }
+
+//        CUSTOMER('C'),
+//    MANAGER('M');
+//
+//    private char value;
+//
+//    private Role(char role) {
+//        switch (role){
+//            case 'M':
+//                setValue('M');
+//                break;
+//            case 'C':
+//                setValue('C');
+//                break;
+//            default:
+//                throw new IllegalArgumentException("Incorrect use of Role");
+//        }
+//    }
+//
+//    public char getValue() {
+//        return value;
+//    }
+//
+//    public void setValue(char value) {
+//        this.value = value;
+//    }
+//
+//    public static class Values {
+//        public static final String CUSTOMER= "C";
+//        public static final String MANAGER= "M";
+//    }
 
 }
